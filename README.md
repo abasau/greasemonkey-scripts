c40c70350f64c790645f3a23411d9ba2611f1cd6
Aliaksandr Basau <abasau@plex.com>
2019-08-11 18:14:04 -0400

    Added an empty script for YouTube

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Repository for Greasemonkey scripts
 [Indeed.com](https://github.com/abasau/greasemonkey-scripts/raw/master/src/indeed.user.js)
 
 [Apartments.com](https://github.com/abasau/greasemonkey-scripts/raw/master/src/apartments.user.js)
+
+[YouTube.com](https://github.com/abasau/greasemonkey-scripts/raw/master/src/youtube.user.js)
---
+++ src/youtube.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name            YouTube.com mass hiding of recommended videos
+// @description     Provides an option on YouTube to mass hide of recommended videos
+// @include         http*://*youtube.tld/*
+// @downloadURL     https://github.com/abasau/greasemonkey-scripts/raw/master/src/youtube.user.js
+// @homepageURL     https://github.com/abasau/greasemonkey-scripts
+// @version         1.0
+// @grant           none
+// ==/UserScript==
